997380b37eb036bf783088ca7b17112b9a336e04
chand1012 <chandlerl14@outlook.com>
2017-08-14 13:38:40 -0400

    moved functions around

--- login_lib.py
+++ login_lib.py
@@ -1,4 +1,6 @@
 from json import loads
+import imaplib
+import email
 def email_login_info(file_name='login.json'):
     try:
         file_keys = open(file_name)
@@ -15,3 +17,27 @@ def email_login_info(file_name='login.json'):
     parsed_json = loads(file_keys.read())
 
     return parsed_json
+
+def get_latest_email(username, epass):
+    FROM_EMAIL  = username
+    FROM_PWD    = epass
+    SMTP_SERVER = "imap.gmail.com"
+    SMTP_PORT   = 993
+
+    mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+    mail.login(FROM_EMAIL,FROM_PWD)
+
+    mail.select('inbox')
+    etype, data = mail.search(None, 'ALL')
+    mail_ids = data[0]
+    id_list = mail_ids.split()
+    first_email_id = int(id_list[0])
+    latest_email_id = int(id_list[-1])
+    typ, data = mail.fetch(str(latest_email_id), '(RFC822)' )
+
+    for response_part in data:
+        if isinstance(response_part, tuple):
+            msg = email.message_from_string(response_part[1].decode('utf-8'))
+            return [msg, first_email_id, latest_email_id]
+
+#add unseen function
--- main.py
+++ main.py
@@ -4,37 +4,14 @@ from flask import Flask
 from login_lib import *
 # we will need this later
 # import requests
-import imaplib
-import email
 name = "Arduino Activator"
 imap_ssl_host = 'imap.gmail.com'
 imap_ssl_port = 993
-last = 0
-current = 0
 logins = email_login_info()
+current = 0
+global last
 user = logins['email']
 password = logins['password']
-def get_latest_email(username, epass):
-    FROM_EMAIL  = username
-    FROM_PWD    = epass
-    SMTP_SERVER = "imap.gmail.com"
-    SMTP_PORT   = 993
-
-    mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-    mail.login(FROM_EMAIL,FROM_PWD)
-
-    mail.select('inbox')
-    etype, data = mail.search(None, 'ALL')
-    mail_ids = data[0]
-    id_list = mail_ids.split()
-    first_email_id = int(id_list[0])
-    latest_email_id = int(id_list[-1])
-    typ, data = mail.fetch(str(latest_email_id), '(RFC822)' )
-
-    for response_part in data:
-        if isinstance(response_part, tuple):
-            msg = email.message_from_string(response_part[1].decode('utf-8'))
-            return [msg, first_email_id, latest_email_id]
 
 app = Flask(name)
 
@@ -43,14 +20,17 @@ def hello_world():
     return "WIP!"
     #this will be used to house the standalone app webpage that will allow for
     #control from a chrome "app"
+
 @app.route('/incoming', methods=['GET', 'POST'])
-def more():
+def more(): # needs fixed to eliminate duplicates
     latest_email = get_latest_email(user, password)
     thing = latest_email[0]
     current = latest_email[2]
     subject = thing['subject']
     if 'Coffee' in subject and last != current:
         print("Making coffee!")
+        return "Making Coffee"
         last = current
     else:
         print("Not making coffee!")
+        return "Not making coffee"
